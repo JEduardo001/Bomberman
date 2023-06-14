ed8900b5e46a1246daaeeb9d5315b1ef2f123103
JEduardo001 <125694974+JEduardo001@users.noreply.github.com>
2023-06-14 12:08:18 -0600

    Update Bomba.java

--- Bomberman/src/Bomberman/Bomba.java
+++ Bomberman/src/Bomberman/Bomba.java
@@ -3,16 +3,19 @@ package Bomberman;
 import java.awt.Image;
 
 public class Bomba {
-	int x,y,largo,alto,tiempo;
+	int x,y,largo,alto,tiempo,direccion;
+	boolean moverBomba;
 	Image imagen;
 	
 	
-	public Bomba(int x,int y,int largo,int alto,Image imagen,int tiempo) {
+	public Bomba(int x,int y,int largo,int alto,Image imagen,int tiempo,int direccion,boolean moverBomba) {
 		this.x=x;
 		this.y=y;
 		this.largo=largo;
 		this.alto=alto;
 		this.imagen=imagen;
 		this.tiempo=tiempo;
+		this.direccion=direccion;
+		this.moverBomba=moverBomba;
 	}
 }
